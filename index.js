b621a7035d8bcac5d6ac25e2b61d24f90dce3918
Cengiz Bugra Aksakal <aksakalcengiz@hotmail.com>
2022-04-15 15:19:20 +0300

    ArrayFilterMethod lab completed.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(element => element.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(element => element[0].toLowerCase() === name[0].toLowerCase());
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(element => element.name.toLowerCase() === name.toLowerCase());
+}
